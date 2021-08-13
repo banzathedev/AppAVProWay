78ef23e361110360a42d85911e9bcdd6e9dac4ea
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-08-13 16:10:37 -0300

    Project SetUp

---
+++ app/src/main/java/com/proway/appav/fragments/UserSettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.proway.appav.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.proway.appav.R
+
+class UserSettingsFragment : Fragment(R.layout.fragment_user_settings) {
+
+//        fun newInstance(param1: String, param2: String) =
+//            UserSettingsFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+}
