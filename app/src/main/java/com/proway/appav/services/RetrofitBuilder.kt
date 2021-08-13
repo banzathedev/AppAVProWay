6ce416b44c2c4072e49ff04404d4f8265b083ea3
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-08-13 16:36:52 -0300

    User Service Creation

--- app/src/main/java/com/proway/appav/services/RetrofitBuilder.kt
+++ app/src/main/java/com/proway/appav/services/RetrofitBuilder.kt
@@ -1,16 +1,21 @@
 package com.proway.appav.services
 
-import com.proway.appav.model.Products
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
-    private val retrofitProducts = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
         .baseUrl("https://fakestoreapi.com")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
     fun getProducts(): ProductsService {
-        return retrofitProducts.create(ProductsService::class.java)
+        return retrofit.create(ProductsService::class.java)
+    }
+
+    fun getUser(): UserService {
+        return retrofit.create(UserService::class.java)
     }
 
 
---
+++ app/src/main/java/com/proway/appav/services/UserService.kt
@@ -0,0 +1,11 @@
+package com.proway.appav.services
+
+import com.proway.appav.model.User
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface UserService {
+
+    @GET("/user/1") TODO("Conferir retorno desse link")
+    fun getUserEndpoint(): Call<User>
+}
